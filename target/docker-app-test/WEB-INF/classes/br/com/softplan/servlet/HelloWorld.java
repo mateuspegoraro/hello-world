9897058c8be2413853a8feadb3e8fa3f704f3f35
mateus.pegoraro <pegoraro.mateus@gmail.com>
2015-05-26 10:43:07 -0300

    first commit

---
+++ target/docker-app-test/WEB-INF/classes/br/com/softplan/servlet/HelloWorld.java
@@ -0,0 +1,46 @@
+package br.com.softplan.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		
+		// Actual logic goes here.
+	    PrintWriter out = response.getWriter();
+	    out.println("<html>");
+	    out.println("<body>");
+	    out.println("<h1>" + "Hello" + "</h1>");
+	    out.println("</body>");
+	    out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
